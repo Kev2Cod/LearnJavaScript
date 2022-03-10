effbc0de868b5455bfaf84e722504d0a346f0b63
Kevin <kevinity310@gmail.com>
2022-03-10 20:36:22 +0700

    Pengulangan : for

--- angkot1.js
+++ angkot1.js
@@ -1,6 +1,7 @@
-var angkot = 1;
+var jmlAngkot = 10;
+var noAngkot = 1;
 
-while(angkot <= 10){
-    console.log('Angkot No. ' + angkot + ' beroperasi dengan baik')
-    angkot++;
+while(noAngkot <= jmlAngkot){
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.')
+    noAngkot++;
 }
