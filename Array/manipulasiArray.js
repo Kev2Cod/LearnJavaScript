d340ab8b8671d6136f0a17fa50536cc8ca262b8d
Kevin <kevinity310@gmail.com>
2022-03-13 23:28:02 +0700

    Array : ForEach & Map & Sort

--- Array/manipulasiArray.js
+++ Array/manipulasiArray.js
@@ -38,9 +38,49 @@
 
 // 5. Slice
 // slice(awal, akhir)
-var arr = ['Kevin', 'Williams', 'Permana', 'Deku', 'Bakugo'];
+// var arr = ['Kevin', 'Williams', 'Permana', 'Deku', 'Bakugo'];
 
-var arr2 = arr.slice(1,3);
+// var arr2 = arr.slice(1,3);
 
-console.log(arr.join(' - '));
-console.log(arr2.join(' - '));
+// console.log(arr.join(' - '));
+// console.log(arr2.join(' - '));
+
+// 6. forEach
+
+// var angka = [1,2,3,4,5,6,7,8,9];
+// var nama = ['Kevin', 'Williams', 'Permana', 'Deku']
+
+// for(var i = 0; i < angka.length; i++){
+//     console.log(angka[i]);
+// }
+
+// angka.forEach(function(e){
+//     console.log(e)
+// });
+
+// nama.forEach(function(e,i){
+//     console.log('Mahasiswa ke-' + (i+1) + ' adalah : ' + e )
+// });
+
+// 7. Map
+// var angka = [1,7,3,5,9,6,1,2,9];
+// var nama = ['Kevin', 'Williams', 'Permana', 'Deku'];
+
+// var angka2 = angka.map(function(e){
+//     return e * 2;
+// });
+// console.log(angka2.join(' - '));
+
+
+// 8. Sort
+// var angka = [1,7,3,20,9,10,1,2,9];
+// angka.sort(function(a,b){
+//     return a-b;
+// });
+// console.table(angka.join(' - '));
+
+
+
+
+
+
