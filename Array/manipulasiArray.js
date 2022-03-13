0076ef88a6b9410154f9c88fcc86229c506a4e6e
Kevin <kevinity310@gmail.com>
2022-03-13 15:17:18 +0700

    Array : Manipulasi Array

---
+++ Array/manipulasiArray.js
@@ -0,0 +1,35 @@
+// Manipulasi Array
+var arr= [];
+// 1. Menambahkan Array
+// arr = ['Kevin', 'Williams'];
+// arr[2] = 'Permana';
+
+// arr[6] = 'Dian';
+// console.log(arr);
+
+// 2. Menghapus Array
+// arr = ['Kevin', 'Budi', 'Deku', 'Kana'];
+// arr[1] = undefined;
+
+// console.log(arr);
+
+//3. Menampilkan isi Array  
+// arr = ['Kevin', 'Budi', 'Deku', 'Kana'];
+// for(var i = 0; i < arr.length; i++){
+//     console.log('Mahasiswa ke-' + (i+1) + ' : ' + arr[i]);
+// }
+
+// Method pada array
+// 1. Join
+arr = ['Kevin', 'Williams', 'Permana'];
+// console.log(arr.join(' - '));
+
+// 2. Push Pop
+// arr.push('Doddy','fitri);
+// arr.pop();
+
+// 3. unshift & shift
+// arr.unshift('Doddy');
+// arr.shift();
+
+console.log(arr.join(' - '));
